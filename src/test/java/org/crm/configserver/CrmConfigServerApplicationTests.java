c7b802c82d66edefd648e40974c2c8938b7bf64e
lachgar03 <simolachgar10@gmail.com>
2025-11-20 12:17:37 +0100

    serv

---
+++ src/test/java/org/crm/configserver/CrmConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crm.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
